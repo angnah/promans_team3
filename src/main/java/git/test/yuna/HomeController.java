86aeb5ead84f472604fceb51c2305656154e6807
angnah <youuuuun9520@naver.com>
2021-09-02 00:46:30 +0900

    this is test files!

---
+++ src/main/java/git/test/yuna/HomeController.java
@@ -0,0 +1,34 @@
+package git.test.yuna;
+
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * Handles requests for the application home page.
+ */
+@Controller
+public class HomeController {
+	
+	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+	
+	
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String home(Locale locale, Model model) {
+		logger.info("Welcome home! The client locale is {}.", locale);
+		
+		System.out.println("깃허브 연결 테스트 !");
+		
+		
+		
+		return "home";
+	}
+	
+}
