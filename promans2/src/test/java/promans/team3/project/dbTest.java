aacfc21e8389f4f0d68936422ef65ee6b7753989
mang-ji <alswlsms555@gmail.com>
2021-09-02 10:00:46 +0900

    야야 해줘

--- promans2/src/test/java/promans/team3/project/dbTest.java
+++ promans2/src/test/java/promans/team3/project/dbTest.java
@@ -17,7 +17,7 @@ public class dbTest {
 			String url;
 			Connection connection = DriverManager.getConnection(info,id,pass);
 			
-			System.out.println("맹지 테스트");
+			System.out.println("맹지 테스트 다쉬");
 			
 		} catch (Exception e) {e.printStackTrace();}
 		
