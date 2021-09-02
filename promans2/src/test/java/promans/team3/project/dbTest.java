0de7c707966c0638ced56fb8de016ca42549999e
ckdrbs <ckdrbssla@naver.com>
2021-09-02 10:17:32 +0900

    ㅁㅈㅇ

--- promans2/src/test/java/promans/team3/project/dbTest.java
+++ promans2/src/test/java/promans/team3/project/dbTest.java
@@ -17,7 +17,7 @@ public class dbTest {
 			String url;
 			Connection connection = DriverManager.getConnection(info,id,pass);
 			
-			System.out.println("성공");
+			System.out.println("푸시");
 			
 		} catch (Exception e) {e.printStackTrace();}
 		
