61c55efcdfb612e8f0b4eccc7241b9e23e967079
mang-ji <alswlsms555@gmail.com>
2021-09-02 10:42:02 +0900

    first commit mang~

---
+++ promans_team3/promans2/src/test/java/promans/team3/project/dbTest.java
@@ -0,0 +1,27 @@
+package promans.team3.project;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import org.junit.Test;
+
+public class dbTest {
+	
+	@Test
+	public void dbTest() {
+		String info = "jdbc:oracle:thin:@192.168.0.253:1521:xe";
+		String id = "ANGNAH";
+		String pass = "1234";
+		
+		try {
+			Class.forName("oracle.jdbc.OracleDriver");
+			String url;
+			Connection connection = DriverManager.getConnection(info,id,pass);
+			
+			System.out.println("성공");
+			
+		} catch (Exception e) {e.printStackTrace();}
+		
+	}
+	
+	
+}
